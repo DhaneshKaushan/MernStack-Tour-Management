4893f74a01dd8082e867df080c2424fc434b70be
dhaneshkaushan <dhaneshkaushan@gmail.com>
2024-08-31 13:28:52 +0530

    create header and create home hero section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from "react";
+import "../styles/home.css";
+
+import { Col, Container, Row, col } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png";
+import Subtitle from "../shared/Subtitle";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle subtitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Traveling opens the door to creating{" "}
+                  <span className="highlight">memories</span>
+                </h1>
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s, when an unknown
+                  printer took a galley of type and scrambled it to make a type
+                  specimen book.
+                </p>
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className="hero__img-box">
+                <img src={heroImg} alt=""/>
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className="hero__img-box">
+                <video src={heroVideo} alt="" controls/>
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className="hero__img-box">
+                <img src={heroImg02} alt=""/>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
 
-export default Home
+export default Home;
