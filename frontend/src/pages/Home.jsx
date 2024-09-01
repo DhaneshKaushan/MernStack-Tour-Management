849ad38605c1e5ada5bcff298886b94271df90b5
dhaneshkaushan <dhaneshkaushan@gmail.com>
2024-09-01 17:58:04 +0530

    create featured tour section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -9,6 +9,7 @@ import worldImg from "../assets/images/world.png";
 import Subtitle from "../shared/Subtitle";
 import SearchBar from "../shared/SearchBar";
 import ServiceList from "../services/ServiceList";
+import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
 
 const Home = () => {
   return (
@@ -65,6 +66,19 @@ const Home = () => {
           </Row>
         </Container>
       </section>
+      {/* ========= featured tour section start ========== */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg ="12" className="mb-5">
+              <Subtitle subtitle={"Explore"}/>
+              <h2 className="featured__tour-title">Our featured tours</h2>
+            </Col>
+            <FeaturedTourList/>
+          </Row>
+        </Container>
+      </section>
+      {/* ========= featured tour section end========== */}
     </>
   );
 };
