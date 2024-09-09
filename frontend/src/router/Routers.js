fcb6f42eb615ceeb2fa7a64232c9f23dc0b29bf5
Dananjaya AKT <dananjayatharindu794@gmail.com>
2024-09-10 02:04:56 +0530

    Add About page and styles

--- frontend/src/router/Routers.js
+++ frontend/src/router/Routers.js
@@ -7,12 +7,14 @@ import ToursDetails from './../pages/TourDetails';
 import Login from './../pages/Login';
 import Register from './../pages/Register';
 import SearchResultList from './../pages/SearchResultList';
+import About from './../pages/About';
 
 const Routers = () => {
   return (
     <Routes>
         <Route path='/' element={<Navigate to='/home' />} />
         <Route path='/home' element={<Home />} />
+        <Route path='/about' element={<About />} />
         <Route path='/tours' element={<Tours />} />
         <Route path='/tours/:id' element={<ToursDetails />} />
         <Route path='/login' element={<Login />} />
