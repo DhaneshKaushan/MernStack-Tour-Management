214aec23ba16578ca3be359f53aeea8e12714e05
Dhanesh Kaushan <dhaneshkaushan@gmail.com>
2024-09-10 23:21:44 +0530

    create booking controller

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -55,7 +55,7 @@ export const deleteTour = async (req, res) => {
 export const getSingleTour = async (req, res) => {
   const id = req.params.id;
   try {
-    const tour = await Tour.findById(id);
+    const tour = await Tour.findById(id).populate("reviews");
     if (!tour) {
       return res.status(404).json({
         success: false,
@@ -79,6 +79,7 @@ export const getAllTour = async (req, res) => {
   const page =parseInt(req.query.page);
   try {
     const tours = await Tour.find({})
+    .populate("reviews")
     .skip(page * 8)
     .limit(8);
     res.status(200).json({
@@ -103,7 +104,7 @@ export const getTourBySearch =async(req,res)=>{
     try {
         //gte means greater than equal
         const tours = await Tour.find({city, distance:{$gte:distance},
-        maxGroupSize:{$gte:maxGroupSize}})
+        maxGroupSize:{$gte:maxGroupSize}}).populate("reviews");
 
         res.status(200).json({
             success: true,
@@ -122,7 +123,7 @@ export const getTourBySearch =async(req,res)=>{
 
 export const getFeaturedTour = async (req, res) => {
   try {
-    const tours = await Tour.find({ featured: true }).limit(8);
+    const tours = await Tour.find({ featured: true }).populate("reviews").limit(8);
     res.status(200).json({
       success: true,
       message: "Tours retrieved successfully",
